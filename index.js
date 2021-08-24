df6dc5124b429f103485bc0cd2c74351a4c02314
derek <derekle.creative@gmail.com>
2021-08-24 01:32:37 -0500

    commit tree 914c582ab24bda7a00e3314eb60baf3b13022e79

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon {
+    constructor(a) {
+        this.a = a;
+      }
+    get countSides() {
+        return this.a.length;
+    }
+    get perimeter(){
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return this.a.reduce(reducer)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        let sides = [
+        this.a[0] + this.a[1],
+        this.a[0] + this.a[2],
+        this.a[1] + this.a[2]
+        ]
+
+        for (const [i, value] of sides.entries()) {
+            sides[i] = !this.a.some((x) => value < x)
+        }
+        
+        return !sides.includes(false)
+    }
+}
+
+class Square extends Polygon{
+    get area(){
+        return Math.pow(this.a[0], 2)
+    }
+    get isValid(){
+        let unique = [...new Set(this.a)];
+        return unique.length == 1
+    }
+}
